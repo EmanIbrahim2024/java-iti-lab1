f7c6028766aeac5454f6761bc4eeca5973bc7298
EmanIbrahim2024 <eng.emanibrahim2024@gmail.com>
2024-12-08 15:00:22 +0200

    version3-lab1

--- script.js
+++ script.js
@@ -28,21 +28,21 @@ BirthOfUser=parseInt(BirthOfUser);
 console.log(BirthOfUser);
 
 
-while( !parseInt(BirthOfUser) || parseInt(BirthOfUser) >= 2010){
+while( !BirthOfUser || BirthOfUser >= 2010){
 BirthOfUser=prompt("Enter you Birthyear :");
 }
 userAge = 2024-BirthOfUser;
-console.log("The age of user is: "+userAge+" years old");
+console.log("The age of user is: "+userAge+" ");
 
 document.write("<p> <b><u> Name </u></b>: "+nameOfUser+" </p> ");
 document.write("<p> <b><u> BirthYear </u></b>: "+BirthOfUser+" </p> ");
-document.write("<p> <b><u> Age </u></b>: "+userAge+" years-old </p> ");
+document.write("<p> <b><u> Age </u></b>: "+userAge+"</p> ");
 
 //////////////ex4////////////////
 
 function printWelcome(){
 for (var i=1;i<7;i++){
-    document.write(" <h"+i+"> Welcome to my site "+i+" </h"+i+">  ")
+    document.write(" <h"+i+"> Welcome to my Page "+i+" </h"+i+">  ")
 }
 }
 printWelcome();
